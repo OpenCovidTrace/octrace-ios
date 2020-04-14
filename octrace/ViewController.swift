8b49486345ce1d5409358b8e05d64b460a3f06a6
Alexander Ivanov <alexander.n.ivanov@yandex.ru>
2020-04-14 22:06:25 +0300

    Initial Commit

---
+++ octrace/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  octrace
+//
+//  Created by Alexander Ivanov on 14.04.2020.
+//  Copyright © 2020 OpenCovidTrace. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
