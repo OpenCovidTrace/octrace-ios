1f77e3348ee6487a2825fa423767fe840703c22f
Andrius Shiaulis <Shiaulis@users.noreply.github.com>
2020-05-01 14:55:18 +0300

    Move MapFeature to MapViewController extension in separate file

--- octrace/UI/Controllers/MapViewController.swift
+++ octrace/UI/Controllers/MapViewController.swift
@@ -384,20 +384,3 @@ extension MKMapView {
     }
     
 }
-
-
-struct MapFeatureValue: Codable {
-    let features: [MapFeature]
-}
-
-struct MapFeature: Codable {
-    let attributes: MapAttribute
-}
-
-struct MapAttribute: Codable {
-    let ADM0_NAME: String
-    let CENTER_LAT: Double?
-    let CENTER_LON: Double?
-    let cum_conf: Int
-    let cum_death: Int
-}
